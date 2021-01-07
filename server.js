101f67ef078e8954ff6f9112d1a66deb88f99d80
Jeff <jeffrey.cheng132@gmail.com>
2021-01-06 22:09:21 -0500

    heroku

--- server.js
+++ server.js
@@ -9,24 +9,14 @@ const routes = require('./routes/routes')
 
 dotenv.config();
 
-const connectDB = async () => {
-	try {
-		 const conn = await mongoose.connect(process.env.MONGO_URI, {
-				useNewUrlParser: true,
-				useCreateIndex: true,
-				useUnifiedTopology: true
-		 });
-		 console.log('hi')
-
-		 console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
-	} catch (err) {
-		 console.log(`Error: ${err.message}`.red);
-		 process.exit(1);
-	}
-}
-
-connectDB();
-
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+}, (err) => {
+	if (err) throw err;
+	console.log(`MongoDB Connected`);
+});
 
 const app = express();
 app.use(express.json());
@@ -34,6 +24,13 @@ app.use(cors());
 
 if(process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
+}
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname,'/client/public/index.html'));
+	});
 }
 
 app.use('/api', routes)
