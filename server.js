febbced0bda5575d8f6333a551c0aac3cccecb07
Jeff <jeffrey.cheng132@gmail.com>
2021-01-06 22:53:41 -0500

    test

--- server.js
+++ server.js
@@ -29,10 +29,7 @@ if(process.env.NODE_ENV === 'development') {
 
 if (process.env.NODE_ENV === "production") {
 	console.log(path.join(__dirname,'/client/public/index.html'))
-	app.use(express.static("build"));
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname,'/client/public/index.html'));
-	});
+	app.use('/static', express.static(path.join(__dirname, 'client/public/')));
 }
 
 app.use('/api', routes)
