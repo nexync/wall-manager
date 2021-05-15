927aa0d905845337c1bb172284340d49115f09c4
Jeffrey Cheng <imjeffrey23@gmail.com>
2021-05-15 19:42:41 -0400

    button

--- client/src/components/layout/Comment.js
+++ client/src/components/layout/Comment.js
@@ -3,22 +3,32 @@ import React, {useContext} from 'react'
 import {GlobalContext} from '../../context/GlobalState'
 
 import {Row, Col} from 'antd'
+import {Button} from 'antd';
+import { CloseOutlined } from '@ant-design/icons';
 
 export const Comment = ({comment}) => {
 	const {currUser} = useContext(GlobalContext)
 
 	const {users} = useContext(GlobalContext)
 	return (
-		<Row style = {currUser.user.displayname == users[comment.createdBy].displayname ? {padding: 15, backgroundColor: 'lightyellow'} : {padding: 15}}>
+		<Row style = {currUser.user.displayname === users[comment.createdBy].displayname ? {padding: 15, backgroundColor: 'lightyellow'} : {padding: 15}}>
 			<Col span = {4}>
 				{users[comment.createdBy].displayname}
 			</Col>
-			<Col span = {18}>
-				{comment.text}
-			</Col>
-			<Col span = {2}>
-				Del
-			</Col>
+			
+			{currUser.user.displayname === users[comment.createdBy].displayname ?
+				<>
+					<Col span = {18}>
+						{comment.text}
+					</Col>
+					<Col span = {2}>
+						<Button ghost = 'true' style = {{color: 'black'}} icon={<CloseOutlined/>} size = 'small' type = "default" />
+					</Col>
+				</> : 
+				<Col span = {20}>
+					{comment.text}
+				</Col>
+			}
 			
 		</Row>
 	)
--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -2,98 +2,24 @@ import React, { useState, useEffect, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { GlobalContext } from "../../context/GlobalState";
 
-import {Button} from 'antd'
+import {Button, Form, Input} from 'antd'
 
 export default function ProfilePage() {
-	const [name, setName] = useState("");
-	const [file, setFile] = useState(null);
-	const [photoURL, setPhotoURL] = useState("");
-	const {currUser, users} = useContext(GlobalContext)
+	const {currUser, users} = useContext(GlobalContext);
+  const [name, setName] = useState("");
   const history = useHistory();
 
-  useEffect(() => {
-    const prefillData = async () => {
-      try {
-				const user = users[currUser.user.id]
-				
-				console.log(user)
-
-        setName(user.displayname);
-        // setPhotoURL(profileDoc.data().photoURL);
-      } catch (err) {
-        console.log(err.message);
-      }
-
-    };
-
-    prefillData();
-  }, []);
-
-  // function handleChange(e) {
-  //   setFile(e.target.files[0]);
-  // }
-
-  // function handleUpload(e) {
-  //   e.preventDefault();
-
-  //   return new Promise((resolve, reject) => {
-  //     const uploadTask = storage.ref(`/images/${file.name}`).put(file);
-  //     uploadTask.on("state_changed", console.log, console.error, () => {
-  //       storage
-  //         .ref("images")
-  //         .child(file.name)
-  //         .getDownloadURL()
-  //         .then(url => {
-  //           setFile(null);
-  //           // console.log(url);
-  //           setPhotoURL(url);
-  //           resolve(url);
-  //         });
-  //     });
-  //   });
-  // }
-
-  // const onSubmit = async e => {
-  //   e.preventDefault();
-
-  //   try {
-  //     const uid = firebase.auth().currentUser.uid;
-
-  //     const query = await db
-  //       .collection("profiles")
-  //       .where("user", "==", uid)
-  //       .get();
-
-  //     const profileDoc = query.docs[0];
-
-  //     console.log(name);
-
-  //     let url = photoURL;
-  //     if (file !== null) url = await handleUpload(e);
-
-  //     await profileDoc.ref.update({
-  //       name: name,
-  //       status: status,
-  //       photoURL: url
-  //     });
-
-  //     console.log(profileDoc.data());
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-
-  //   history.push("/dashboard");
-  // };
-
-  // if (firebase.auth().currentUser == null) {
-  //   history.push("/");
-  // }
-
   return (
-		<>
-    <p>Under construction sorry :(</p>
-		<Button ghost = 'true' onClick = {() => history.push('/dashboard')}>Back</Button>
-		</>
+		<div className = 'profile-container'>
+      <Form>
+        <Form.Item>
+          <Input placeholder = 'Display Name' type = 'text' onchange = {(e) => setName(e.target.value)}/>
+        </Form.Item>
+      </Form>
+      <p>Under construction sorry :(</p>
+
+      <Button ghost = 'true' onClick = {() => history.push('/dashboard')}>Back</Button>
+		</div>
 	)
 }
 
